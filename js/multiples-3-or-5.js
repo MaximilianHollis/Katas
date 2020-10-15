97e489e76b218e6b907860ef1747311febe1106f
Maximilian Hollis <50596827+MaximilianHollis@users.noreply.github.com>
2020-10-15 13:23:41 -0400

    Create multiples-3-or-5.js

---
+++ js/multiples-3-or-5.js
@@ -0,0 +1,10 @@
+function solution(n){
+  let numbers = [0];
+  for (let i = 0; i < n; i++){
+    if (i % 3 === 0 || i % 5 === 0){
+      numbers[i] = i;
+    }
+  }
+  //remember to put return in inner functions
+  return numbers.reduce((acc, a) => {return acc + a}, 0);
+}

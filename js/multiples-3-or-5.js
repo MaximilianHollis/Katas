9ff3759f63cb72a0b388e667b26b36f3d944f28e
Maximilian Hollis <50596827+MaximilianHollis@users.noreply.github.com>
2020-10-15 13:25:48 -0400

    Update multiples-3-or-5.js

--- js/multiples-3-or-5.js
+++ js/multiples-3-or-5.js
@@ -8,3 +8,15 @@ function solution(n){
   //remember to put return in inner functions
   return numbers.reduce((acc, a) => {return acc + a}, 0);
 }
+
+//Much cleaner
+function solution(n){
+  let numbers = 0;
+  for (let i = 0; i < n; i++){
+    if (i % 3 === 0 || i % 5 === 0){
+      numbers =+ i;
+    }
+  }
+  //remember to put return in inner functions
+  return numbers;
+}

423017d26368081a0f9cfdd40bf2529aff28b73c
Maximilian Hollis <50596827+MaximilianHollis@users.noreply.github.com>
2020-10-15 09:45:34 -0400

    Update Stop-gninnipS-My-sdroW.js
    git rid of redundant function

--- js/Stop-gninnipS-My-sdroW.js
+++ js/Stop-gninnipS-My-sdroW.js
@@ -1,9 +1,4 @@
 function spinWords(w){
   let words = w.split(' ');
-  return words.map((w) => (w.length > 4) ? reverse(w) : w).join(' ');
-}
-            
-function reverse(w){
-  let words = w.split('')
-  return words.reverse().join('');
+  return words.map((w) => (w.length > 4) ? w.split('').reverse().join('') : w).join(' ');
 }

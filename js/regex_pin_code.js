fce354f426e2ed3fa754d3301258d185df899e0e
Maximilian Hollis <50596827+MaximilianHollis@users.noreply.github.com>
2020-10-17 22:30:57 -0400

    Update regex_pin_code.js

--- js/regex_pin_code.js
+++ js/regex_pin_code.js
@@ -3,3 +3,9 @@ function validatePIN (p) {
   p = p.match(/[^0-9]/g);
   return ((a== 4 || a == 6) && !p) ? true : false;
 }
+
+//better solution
+
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin)
+}
